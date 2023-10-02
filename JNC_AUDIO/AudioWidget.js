f7499ee3eb6d46868b0cbb2442713edd80f4289e
juergennoe <juergen-noe-consulting@t-online.de>
2023-10-02 13:57:13 +0200

    remove load D3.js

--- JNC_AUDIO/AudioWidget.js
+++ JNC_AUDIO/AudioWidget.js
@@ -3,8 +3,6 @@
     tmpl.innerHTML = `
           <div class="audio-container"> <audio controls> <source src="https://juergennoe.github.io/sac_widgets/JNC_AUDIO/jnc-sample.m4a" type="audio/mpeg">  </audio></div>   
         <div id="audio_welcome"></div>               
-               </div>
-        </div>
     `;
 
     class JNC_AudioWidget extends HTMLElement {
@@ -13,12 +11,6 @@
             this._shadowRoot = this.attachShadow({mode: 'open'});
             this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this._props = {};
-
-            // Load D3.js
-            const script = document.createElement('script');
-            script.src = 'https://d3js.org/d3.v5.min.js';
-            script.onload = () => this._ready = true;
-            this._shadowRoot.appendChild(script);
         }
 
         onCustomWidgetBeforeUpdate(changedProperties) {
